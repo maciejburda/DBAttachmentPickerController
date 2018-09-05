693e724edbed0d9b0256c0f1f78122e2f48ab416
Maciej Burda <maciej.burda@nestbank.pl>
2018-09-05 11:32:25 +0200

    enforced bundle

--- Source/Categories/NSBundle+DBLibrary.m
+++ Source/Categories/NSBundle+DBLibrary.m
@@ -33,7 +33,9 @@ NSString * const kLocalizedStringNotFound = @"kLocalizedStringNotFound";
 + (instancetype)dbAttachmentPickerResourceBundle {
     NSBundle *bundle = [NSBundle mainBundle];
     NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"DBAttachmentPickerController" ofType:@"bundle"];
-    if (bundlePath) {
+    if (!bundlePath) {
+        bundlePath = [[NSBundle bundleForClass:[DBAttachmentPickerController class]] pathForResource:@"DBAttachmentPickerController" ofType:@"bundle"];
+    else {
         bundle = [NSBundle bundleWithPath:bundlePath];
     }
     return bundle;
